90bab52b3941c4371455aadcadf41b557d70a2ad
Amirhossein Davoody <amirhossein.davoody@gmail.com>
2017-12-20 17:24:53 -0600

    trello clone

--- trello_clone/lib/renderer.js
+++ trello_clone/lib/renderer.js
@@ -1,29 +1,103 @@
-const electron = require('electron')
-const ipc = electron.ipcRenderer // to enable communication between renderer and main process
-const shell = electron.shell
-const marked = require('marked') // render markdown documents
+const electron = require('electron');
+const ipc = electron.ipcRenderer;
 
-const remote = electron.remote
-const mainProcess = remote.require('./main')
-const clipboard = remote.clipboard
+// control buttons
+const controls = document.querySelector('section.controls');
+const newBoardButton = document.querySelector('section.controls > button#new-board');
+const newListButton = document.querySelector('section.controls > button#new-list');
 
-const $ = selector => document.querySelector(selector) // a function named $ that gets a selector as parameter and returns the element
-const $markdownView = $('.raw-markdown')
-const $htmlView = $('.rendered-html')
-const $newBoardButton = $('#new-board')
-const $newListButton = $('#new-list')
-const $newCardButton = $('#new-card')
+// board area
+let board;
+let boards = [];
+const boardList = document.querySelector('section.board-list');
+createNewBoard();
 
-const $content = $('#content')
 
-let currentFile = null
+let numberOfBoards = 0;
+let numberOfLists = [0];
+let numberOfCards = [0];
 
-$newBoardButton.addEventListener('click', () => {
-  console.log("create new board!!")
-  var node = $content.createElement("This is new.");
-  // $content.appendChild(node);
+let currentBoardNumber = 0;
+let currentListNumber = 0;
+let currentCardNumber = 0;
 
-})
+// create new board
+function createNewBoard(){
+  var newBoard = document.createElement('section');
+  if (board){
+    board.style.display = "none";
+    board = board.insertAdjacentElement('beforebegin',newBoard);
+  }
+  else{
+    board = controls.insertAdjacentElement('afterend', newBoard);
+  }
+  boards.push(board);
+  board.style.border = "2px solid rgb(233,171,88)";
+  board.style.padding = "1em";
+  board.style.margin = "5px";
+  board.style.color = "#d9480f";
+  board.style.display = "flex";
+  board.style.overflow = "auto";
+  board.style.minHeight = "220px";
+
+  addBoardList();
+}
+
+// create board-list item
+function addBoardList(){
+  var newDiv = document.createElement('div');
+  var newDiv = boardList.appendChild(newDiv);
+  newDiv.textContent = "board " + boards.length;
+  newDiv.style.background = "rgb(230, 230, 230)";
+  newDiv.style.padding = "5px";
+  newDiv.style.margin = "5px";
+}
+
+// create new list
+function createNewList() {
+  numberOfLists[currentBoardNumber] += 1;
+  var newDiv = document.createElement('div');
+  var newList = board.appendChild(newDiv);
+  newList.textContent = "list " + numberOfLists[currentBoardNumber];
+  newList.style.background = "rgb(230, 230, 230)";
+  newList.style.padding = "5px";
+  newList.style.margin = "5px";
+  newList.style.width = "100px";
+  newList.style.minWidth = "100px";
+  newList.style.minHeight = "200px";
+  newList.style.overflow = "auto";
+}
+
+newBoardButton.addEventListener('click', createNewBoard);
+newListButton.addEventListener('click', createNewList);
+
+
+// const electron = require('electron')
+// const ipc = electron.ipcRenderer // to enable communication between renderer and main process
+// const shell = electron.shell
+// const marked = require('marked') // render markdown documents
+
+// const remote = electron.remote
+// const mainProcess = remote.require('./main')
+// const clipboard = remote.clipboard
+
+// const $ = selector => document.querySelector(selector) // a function named $ that gets a selector as parameter and returns the element
+// const $markdownView = $('.raw-markdown')
+// const $htmlView = $('.rendered-html')
+// const $newBoardButton = $('#new-board')
+// const $newListButton = $('#new-list')
+// const $newCardButton = $('#new-card')
+
+// const $content = $('#content')
+
+// let currentFile = null
+
+// $newBoardButton.addEventListener('click', () => {
+//   console.log("create new board!!")
+//   var node = $content.createElement("This is new.");
+//   // $content.appendChild(node);
+
+// })
 
 
 // ipc.on('file-opened', (event, file, content) => {
